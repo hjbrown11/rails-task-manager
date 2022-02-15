b4286f54b5570ad3d0885d10abf4f1234a380d90
Hannah Brown <hannah.j.brown@hotmail.co.uk>
2022-02-15 16:31:15 +0100

    Pushing changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'tasks', to: 'tasks#index'
-  get 'tasks/:id', to: 'tasks#show', as: :task
+  resources :tasks
+  # # create a task
+  # get 'tasks/new', to: 'tasks#new'
+  # post 'tasks', to: 'tasks#create'
+
+  # # show all tasks
+  # get 'tasks', to: 'tasks#index', as: :tasks_index
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # # edit existing task
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch 'tasks/:id', to: 'tasks#update'
+
+  # # delete a task
+  # delete 'tasks/:id', to: 'tasks#destroy'
 end
